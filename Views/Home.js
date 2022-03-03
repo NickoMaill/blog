0c7bbdd6eca4e7d6cf0cc29157433621c031365c
Nicko <nickos@iMac.local>
2022-03-03 11:44:48 +0100

    testing fecth post user1

--- Views/Home.js
+++ Views/Home.js
@@ -1,6 +1,16 @@
 import { SafeAreaView, Text } from "react-native";
+import { useEffect } from "react";
 
 export default function Home() {
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users/1/posts")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    }, []);
+
     return (
         <SafeAreaView>
 
